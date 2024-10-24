8bc45f7dba2621f635fcacf67e37174cad6810d7
Sibar Al-ani <sibar.alani@gmail.com>
2024-10-24 09:14:14 +0200

    tests

--- js/main.js
+++ js/main.js
@@ -21,30 +21,24 @@ function rollGame(event) {
   event.preventDefault();
 
   // Rondom number between 0-36
-  const random = Math.floor(Math.random() * 37) + 0;
-  const color  = table[random];
+  const randomNumber = Math.floor(Math.random() * 37) + 0;
+  const numberColor  = table[randomNumber];
 
-  console.log(`Number: ${random}, color: ${table[random]}`)
+  // 1. Skapa korrekt logik för att bestämma om spelaren vann eller ej
+  // - Jämför den valda färgen från rulllistan selectedColor.value med numberColor. Om de är lika, Då har spelaren vunnit, annars har han förlorat
+
+  console.log(selectedColor.value);
+
+  // 2. Justera bankroll och vissa korrekt meddelande i log, beroende om Spelaren Vann eller inte
+  // - Om spelaren vann, ge meddelandet via log, i en av if/else-blocket
+  // - Om spelade förlorade, ge ett annat meddelande via log, i den andra if/else-blocket
+
+  console.log(`Number: ${randomNumber}, color: ${numberColor}`)
 
   console.log('You just rolled :)')
   const logMessage = `<p>Bet is ??? spinning the wheel...Stopped at ?????. You won/lost</p>`
   log.innerHTML = logMessage + log.innerHTML;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /**
@@ -54,7 +48,7 @@ function rollGame(event) {
 // 2. Justera bankroll och vissa korrekt meddelande i log, beroende om Spelaren Vann eller inte
 // 3. Errorhantering: Lägg till validering på textfälten, så att endast nummer kan anges i textfältet, annars visa ett felmeddelande
 // 4. Errorhantering: Se till att man ej kan satsa mer än vad man har i bankroll. Dvs undvik att spelaren kan spela med minus i bankroll
-// 5. Avancerat: Skapa ett GUI där du via en loop generar alla element för ett visuellt Roulette bord.
+// 5. Avancerat: Skapa ett GUI där du via en loop generar alla element för ett visuellt skapa Roulette bordet.
 // 6. Avancerat: Se till att animera så att för för varje "Roll", det hoppar mellan 10 random tal innan det landar på ett tal
 // 7. Avancerat: Refactorera all kod till mindre funktioner
 // 8. Avancerat: Skapa logik för att kunna spela/betta på siffror, och inte bara färg 
